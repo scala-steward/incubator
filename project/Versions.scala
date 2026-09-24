792bd7be90f8a99e74578a0034249d4c08931496
Scala Steward <scala_steward@virtuslab.com>
2026-09-24 15:14:23 +0000

    Update neo4j-java-driver to 6.3.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val config     = "1.4.9"
   val circe      = "1.15.0"
   val h2         = "2.5.250"
-  val neo4j      = "6.2.1"
+  val neo4j      = "6.3.0"
   val playJson   = "2.10.6"
   val postgres   = "42.7.13"
   val scalatest  = "3.2.20"
